12064c446a1ccba2e1a60c9f200712fc55fa77fb
kanikarun <kanikarun0@hmail.com>
2026-02-27 20:39:29 +0700

    add login register

--- app.py
+++ app.py
@@ -157,5 +157,12 @@ def about_us():
 @app.route('/franchise')
 def franchise():
     return render_template('franchise.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 if __name__ == '__main__':
     app.run(debug=True)
